2b0ab88b2798759eb9ec529a327c1253fe26005b
Dmitry S <dmitry.s@hsdn.org>
2022-08-10 23:16:31 +0400

    Add settings lock

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 Saves your interface and chat settings in local storage, preventing bad settings from being loaded from the server.
 
 To save the settings after changing, exit the character selection screen or correct exit the game. When you incorrect close the game, the current settings will not be saved.
+
+## Lock Settings
+
+Use command `lock` to lock settings. To unlock the settings, re-enter the command.
+
+Important: if you have changed the settings and want to lock them, first go to the character selection screen, then return to the game and enter the command `lock`.
--- index.js
+++ index.js
@@ -12,21 +12,21 @@ module.exports = function saver(mod) {
 	mod.hook("S_REPLY_CLIENT_CHAT_OPTION_SETTING", "raw", sLoadClientSetting.bind(null, "chatSettings"));
 	mod.hook("C_SAVE_CLIENT_ACCOUNT_SETTING", "raw", cSaveClientSetting.bind(null, "accountSettings"));
 	mod.hook("C_SAVE_CLIENT_CHAT_OPTION_SETTING", "raw", cSaveClientSetting.bind(null, "chatSettings"));
+	mod.hook("S_EXIT", "raw", { "filter": { "fake": null } }, sExit.bind(null));
+	mod.hook("S_DECO_UI_INFO", "raw", sExit.bind(null));
 
-	mod.hook("S_EXIT", "raw", { "filter": { "fake": null } }, () => {
-		saveData({ ...getData(), ...dataToSave });
-	});
-
-	mod.hook("S_DECO_UI_INFO", "raw", () => {
-		if (Object.keys(dataToSave).length !== 0) {
-			saveData({ ...getData(), ...dataToSave });
-		}
+	mod.command.add("setlock", () => {
+		const jsonData = getData();
+		jsonData.lock = !jsonData.lock;
+		saveData(jsonData);
+		mod.command.message(`Your settings is ${jsonData.lock ? "locked" : "unlocked"}.`);
 	});
 
 	function sLoadClientSetting(key, opcode, payload, incoming, fake) {
 		const jsonData = getData();
-		if (jsonData && jsonData[key] && !fake) {
-			mod.toClient(Buffer.from(`${jsonData[key].length}${getPacketInfo(payload).opcode}${jsonData[key].payload}`, "hex"));
+		if (jsonData[key] && !fake) {
+			mod.toClient(Buffer.from(`${jsonData[key].length}${
+				getPacketInfo(payload).opcode}${jsonData[key].payload}`, "hex"));
 			return false;
 		}
 	}
@@ -35,14 +35,29 @@ module.exports = function saver(mod) {
 		dataToSave[key] = getPacketInfo(payload);
 	}
 
+	function sExit() {
+		const jsonData = getData();
+
+		if (Object.keys(dataToSave).length !== 0) {
+			saveData(jsonData.lock ?
+				{ ...dataToSave, ...jsonData, "lock": jsonData.lock } :
+				{ ...jsonData, ...dataToSave, "lock": jsonData.lock }
+			);
+		}
+	}
+
 	function getData() {
 		try {
-			return JSON.parse(fs.readFileSync(path.join(__dirname, "saved", `${mod.game.serverId}-${mod.game.accountId}.json`)));
-		} catch (_) {}
+			return JSON.parse(fs.readFileSync(path.join(__dirname, "saved", `${
+				mod.game.serverId}-${mod.game.accountId}.json`)));
+		} catch (_) {
+			return {};
+		}
 	}
 
 	function saveData(data) {
-		fs.writeFileSync(path.join(__dirname, "saved", `${mod.game.serverId}-${mod.game.accountId}.json`), JSON.stringify(data, null, 4));
+		fs.writeFileSync(path.join(__dirname, "saved", `${
+			mod.game.serverId}-${mod.game.accountId}.json`), JSON.stringify(data, null, 4));
 	}
 
 	function getPacketInfo(payload) {
